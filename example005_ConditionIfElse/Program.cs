f7fa51659927ce558ffb45ff3e057b9dccc817eb
JuliaRomaeva <julia.romaeva@gmail.com>
2023-02-11 19:24:44 +0300

    example005_ConditionIfElse

---
+++ example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "антон")
+{
+    Console.WriteLine("Ура, это же АНТОН!");
+} 
+else 
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
